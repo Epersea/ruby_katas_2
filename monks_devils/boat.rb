90ca2f4b09dd56ce20535f696cc03d3fda63f400
elisafvic <elisapersea@gmail.com>
2024-04-05 17:39:52 +0200

    Boat class: when there are no passenger options left

--- monks_devils/boat.rb
+++ monks_devils/boat.rb
@@ -2,12 +2,20 @@ class Boat
   attr_reader :passenger_combinations, :current_passengers
 
   def initialize
-    @passenger_combinations = ['DD', 'MD', 'MM']
+    @passenger_combinations = initial_passenger_combinations
     @current_passengers = []
   end
 
   def select_passengers
     @current_passengers = []
-    @current_passengers << @passenger_combinations.pop
+    if @passenger_combinations.empty?
+      @passenger_combinations = initial_passenger_combinations
+    else
+      @current_passengers << @passenger_combinations.pop
+    end
+  end
+
+  def initial_passenger_combinations
+    ['DD', 'MD', 'MM']
   end
 end
--- monks_devils/spec/boat_spec.rb
+++ monks_devils/spec/boat_spec.rb
@@ -3,12 +3,12 @@ require_relative '../boat'
 RSpec.describe Boat do
 
   let(:boat) { Boat.new }
+  let(:initial_passenger_combinations) {['DD', 'MD', 'MM']}
 
   it 'initiates with 3 passenger combinations and no current passengers' do
     
-    expected_passenger_combinations = ['DD', 'MD', 'MM']
     expected_current_passengers = []
-    expect(boat.passenger_combinations).to eq(expected_passenger_combinations)
+    expect(boat.passenger_combinations).to eq(initial_passenger_combinations)
     expect(boat.current_passengers).to eq(expected_current_passengers)
   end
 
@@ -42,5 +42,21 @@ RSpec.describe Boat do
     expect(boat.current_passengers).to eq(expected_current_passengers)
   end
 
+  context 'when there is no passenger combinations left' do
+    it 'current passengers are empty' do
+
+      4.times { boat.select_passengers }
+
+      expect(boat.current_passengers).to be_empty
+    end
+
+    it 'passenger combinations are restarted' do
+
+      4.times { boat.select_passengers }
+
+      expect(boat.passenger_combinations).to eq(initial_passenger_combinations)
+    end
+
+  end
 
 end
