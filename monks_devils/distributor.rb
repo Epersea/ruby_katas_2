d433e01945a11aa60bd45e0e798d580950301545
elisafvic <elisapersea@gmail.com>
2024-04-07 12:17:24 +0200

    If boat has passengers, calls to calculate new state

--- monks_devils/distributor.rb
+++ monks_devils/distributor.rb
@@ -7,13 +7,29 @@ class Distributor
     @initial_state = State.new
     @current_state = {}
     @states_to_try = [@initial_state]
+    @boat = Boat.new
   end
 
   def initiate_trip
     obtain_current_state
+
+    select_boat_passengers
+    
+    unless @boat.current_passengers.empty?
+      calculate_new_state
+    end
+
   end
 
   def obtain_current_state
     @current_state = @states_to_try.pop
+  end
+
+  def select_boat_passengers
+    @boat.select_passengers
+  end
+
+  def calculate_new_state
+
   end
 end
--- monks_devils/spec/distributor_spec.rb
+++ monks_devils/spec/distributor_spec.rb
@@ -2,8 +2,9 @@ require_relative '../distributor'
 require_relative '../state'
 
 RSpec.describe Distributor do
+  let(:distributor) { Distributor.new }
+
   it 'obtains current state' do
-    distributor = Distributor.new
     expect(distributor.states_to_try).not_to be_empty
     expect(distributor.current_state).to be_empty
     
@@ -13,6 +14,14 @@ RSpec.describe Distributor do
     expect(distributor.current_state.shore_2).to be_empty
     expect(distributor.current_state.trips).to be_empty
     expect(distributor.states_to_try).to be_empty
+  end
+
+  it 'if boat has passengers, calls to calculate new state' do
+    allow(distributor).to receive(:calculate_new_state).and_call_original
+
+    distributor.initiate_trip
+
+    expect(distributor).to have_received(:calculate_new_state)
   end
 
   def initial_shore_1
