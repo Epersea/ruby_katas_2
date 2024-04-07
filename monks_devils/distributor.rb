404e349ccd9fb1884e3a01845f4e18c6287b8bc0
elisafvic <elisapersea@gmail.com>
2024-04-07 12:59:15 +0200

    Calculates new state

--- monks_devils/distributor.rb
+++ monks_devils/distributor.rb
@@ -14,11 +14,9 @@ class Distributor
     obtain_current_state
 
     select_boat_passengers
-    
-    unless @boat.current_passengers.empty?
-      calculate_new_state
-    end
 
+    calculate_new_state
+    
   end
 
   def obtain_current_state
@@ -27,9 +25,28 @@ class Distributor
 
   def select_boat_passengers
     @boat.select_passengers
+
+    if @boat.current_passengers.empty?
+      initiate_trip
+    end
   end
 
   def calculate_new_state
+    delete_passengers_from_shore_1
+    add_passengers_to_shore_2
+  end
 
+  def delete_passengers_from_shore_1
+    @boat.current_passengers.each do |passenger|
+      if @current_state.shore_1.include?(passenger)
+        @current_state.shore_1.delete_at(@current_state.shore_1.index(passenger))
+      end
+    end
+  end
+
+  def add_passengers_to_shore_2
+    @boat.current_passengers.each do |passenger|
+      @current_state.shore_2.push(passenger)
+    end
   end
 end
--- monks_devils/spec/distributor_spec.rb
+++ monks_devils/spec/distributor_spec.rb
@@ -24,7 +24,33 @@ RSpec.describe Distributor do
     expect(distributor).to have_received(:calculate_new_state)
   end
 
+  it 'if boat has no passengers, calls initiate trip' do
+    allow(distributor).to receive(:initiate_trip).and_call_original
+
+    4.times {distributor.select_boat_passengers}
+
+    expect(distributor).to have_received(:initiate_trip)
+  end
+
+  it 'calculates new state' do
+    distributor.obtain_current_state
+    distributor.select_boat_passengers
+
+    distributor.calculate_new_state
+
+    expect(distributor.current_state.shore_1).to eq(shore_1_after_first_trip)
+    expect(distributor.current_state.shore_2).to eq(shore_2_after_first_trip)
+  end
+
   def initial_shore_1
     ["D", "D", "D", "M", "M", "M"]
+  end
+
+  def shore_1_after_first_trip
+    ["D", "D", "D", "M"]
+  end
+
+  def shore_2_after_first_trip
+    ["M", "M"]
   end
 end
