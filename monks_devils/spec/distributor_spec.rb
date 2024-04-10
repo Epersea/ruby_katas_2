b5244c019fd1171e6344bbf9a070018bf7c0d2bd
elisafvic <elisapersea@gmail.com>
2024-04-10 13:54:25 +0200

    Fix Distributor tests

--- monks_devils/spec/distributor_spec.rb
+++ monks_devils/spec/distributor_spec.rb
@@ -4,43 +4,29 @@ require_relative '../state'
 RSpec.describe Distributor do
   let(:distributor) { Distributor.new }
 
-  # it 'obtains current state' do
-  #   expect(distributor.states_to_try).not_to be_empty
-  #   expect(distributor.current_state).to be_empty
+  it 'obtains current state' do
+    expect(distributor.states_to_try).not_to be_empty
+    expect(distributor.current_state).to be_empty
     
-  #   distributor.obtain_current_state
+    distributor.obtain_current_state
     
-  #   expect(distributor.current_state.shore_1).to eq(initial_shore_1)
-  #   expect(distributor.current_state.shore_2).to be_empty
-  #   expect(distributor.current_state.trips).to be_empty
-  #   expect(distributor.states_to_try).to be_empty
-  # end
+    expect(distributor.current_state.shore_1).to eq(initial_shore_1)
+    expect(distributor.current_state.shore_2).to be_empty
+    expect(distributor.current_state.trips).to be_empty
+    expect(distributor.states_to_try).to be_empty
+  end
 
-  # it 'if boat has passengers, calls to calculate new state' do
-  #   allow(distributor).to receive(:calculate_new_state).and_call_original
+  it 'calculates new state' do
+    distributor.obtain_current_state
+    boat = Boat.new(initial_shore_1)
+    boat.select_passengers
 
-  #   distributor.initiate_trip
+    new_state = distributor.calculate_new_state(boat)
 
-  #   expect(distributor).to have_received(:calculate_new_state)
-  # end
-
-  # it 'if boat has no passengers, calls initiate trip' do
-  #   allow(distributor).to receive(:initiate_trip).and_call_original
-
-  #   4.times {distributor.select_boat_passengers}
-
-  #   expect(distributor).to have_received(:initiate_trip)
-  # end
-
-  # it 'calculates new state' do
-  #   distributor.obtain_current_state
-  #   distributor.select_boat_passengers
-
-  #   distributor.calculate_new_state
-
-  #   expect(distributor.current_state.shore_1).to eq(shore_1_after_first_trip)
-  #   expect(distributor.current_state.shore_2).to eq(shore_2_after_first_trip)
-  # end
+    expect(new_state.shore_1).to eq(shore_1_after_first_trip)
+    expect(new_state.shore_2).to eq(shore_2_after_first_trip)
+    expect(new_state.trips).to eq(trips_after_first_trip)
+  end
 
   it 'finds correct solution' do
     result = distributor.initiate_trip
@@ -58,5 +44,9 @@ RSpec.describe Distributor do
 
   def shore_2_after_first_trip
     ["M", "M"]
+  end
+
+  def trips_after_first_trip
+    [["M", "M"]]
   end
 end
--- monks_devils/spec/state_spec.rb
+++ monks_devils/spec/state_spec.rb
@@ -2,45 +2,43 @@ require_relative '../state'
 
 RSpec.describe State do
 
-  context 'validation' do
-    it 'returns invalid if there are more devils than monks on shore 1' do
-      state = State.new(['D', 'D', 'M'], ['M', 'M', 'D'])
+  it 'returns invalid if there are more devils than monks on shore 1' do
+    state = State.new(['D', 'D', 'M'], ['M', 'M', 'D'])
 
-      validation = state.validate
+    validation = state.validate
 
-      expect(validation).to eq('INVALID')
-    end
+    expect(validation).to eq('INVALID')
+  end
 
-    it 'returns invalid if there are more devils than monks on shore 2' do
-      state = State.new(['D', 'M', 'M'], ['M', 'D', 'D'])
+  it 'returns invalid if there are more devils than monks on shore 2' do
+    state = State.new(['D', 'M', 'M'], ['M', 'D', 'D'])
 
-      validation = state.validate
+    validation = state.validate
 
-      expect(validation).to eq('INVALID')
-    end
+    expect(validation).to eq('INVALID')
+  end
 
-    it 'returns valid if there are no more devils than monks in either shore' do
-      state = State.new(['D', 'M'], ['M', 'M' 'D', 'D'])
+  it 'returns valid if there are no more devils than monks in either shore' do
+    state = State.new(['D', 'M'], ['M', 'M' 'D', 'D'])
 
-      validation = state.validate
+    validation = state.validate
 
-      expect(validation).to eq('VALID')
-    end
+    expect(validation).to eq('VALID')
+  end
 
-    it 'returns valid if there are only devils in one shore and more monks than devils in the other' do
-      state = State.new(['D', 'D'], ['M', 'M' 'M', 'D'])
+  it 'returns valid if there are only devils in one shore and more monks than devils in the other' do
+    state = State.new(['D', 'D'], ['M', 'M' 'M', 'D'])
 
-      validation = state.validate
+    validation = state.validate
 
-      expect(validation).to eq('VALID')
-    end
+    expect(validation).to eq('VALID')
+  end
 
-    it 'returns finished if all passengers are in shore 2' do
-      state = State.new([], ['D', 'M', 'D', 'M', 'D', 'M'])
+  it 'returns finished if all passengers are in shore 2' do
+    state = State.new([], ['D', 'M', 'D', 'M', 'D', 'M'])
 
-      validation = state.validate
+    validation = state.validate
 
-      expect(validation).to eq('FINISHED')
-    end
+    expect(validation).to eq('FINISHED')
   end
 end
